518c1606b4adc6189a08736f7a6b14f1701eb682
Viktor Berczeli <viktor.berczeli@gmail.com>
2026-02-22 17:13:36 +0100

    Add project files (converted nested repos to normal folders)

---
+++ client/tailwind.config.cjs
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,ts}'],
+  theme: {
+    extend: {
+      colors: {
+        // Your Historical Palette
+        iron: '#434343',
+        parchment: '#fcf5e5',
+        wood: '#5d4037',
+        blood: '#9d2235', // Hungarian Red
+        gold: '#d4af37',
+      },
+      fontFamily: {
+        // You can add medieval-style Google Fonts here later
+        serif: ['Georgia', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+};
