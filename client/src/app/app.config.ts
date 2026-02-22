6d572a08b3836e81517abaeb3aea00d7d641105e
Viktor Berczeli <viktor.berczeli@gmail.com>
2026-02-22 20:18:58 +0100

    refactor: remove Tailwind CSS configuration and integrate new theme setup

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,11 +1,18 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
 
+import Aura from '@primeuix/themes/aura';
+import { providePrimeNG } from 'primeng/config';
+import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+      },
+    }),
+  ],
 };
