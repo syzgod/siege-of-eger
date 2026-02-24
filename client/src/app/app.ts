1013a363d2e7abfad004210cfec6a4a9cf29d3d3
Viktor Berczeli <viktor.berczeli@gmail.com>
2026-02-24 21:24:15 +0100

    feat: update Angular configuration and environment settings

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -2,6 +2,7 @@ import { Component, resource, signal } from '@angular/core';
 
 import { RouterOutlet } from '@angular/router';
 import { GameStateSchema, type GameState } from '@shared/models/game-state.schema';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -14,7 +15,7 @@ export class App {
 
   gameData = resource({
     loader: async (): Promise<GameState> => {
-      const response = await fetch('http://localhost:3000/game/resources');
+      const response = await fetch(`${environment.apiUrl}/game/resources`);
       const data: unknown = await response.json();
       return GameStateSchema.parse(data);
     },
