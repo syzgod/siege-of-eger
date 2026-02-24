bd7c4b82469bf4ed44f1866049efbf35dd86f067
Viktor Berczeli <viktor.berczeli@gmail.com>
2026-02-24 22:57:58 +0100

    fix: enable CORS for Netlify production URL

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:4200',
+    origin: ['https://siege-of-eger.netlify.app', 'http://localhost:4200'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
